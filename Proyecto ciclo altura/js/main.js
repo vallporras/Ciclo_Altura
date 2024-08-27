87258b3d4bfe6e0aa719c044531407151c65aa42
vallporras <163150359+vallporras@users.noreply.github.com>
2024-08-27 08:05:18 -0600

    Add files via upload

---
+++ Proyecto ciclo altura/js/main.js
@@ -0,0 +1,16 @@
+/*=============== SHOW MENU ===============*/
+const   navMenu= document.getElementById('nav'),
+        navToggle= document.getElementById('nav-toggle'),
+        navClose= document.getElementById('nav-close')
+/*Menu show*/
+if(navToggle){
+    navToggle.addEventListener('click',()=>{
+        navMenu.classList.add('show-menu')
+    })
+}
+//*menu hidden//*
+if(navClose){
+    navClose.addEventListener('click',()=>{
+        navMenu.classList.remove('show-menu')
+});
+}
